1c09b6f4fc09508a9f5e06f178d79065f96dd2c9
PrasadDa08 <prasaddatir05@gmail.com>
2023-06-27 05:44:16 +0530

    Contact Me updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
+  const [result, setResult] = useState()
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+
 
     emailjs.sendForm('gmail', 'template_ffjc3vo', form.current, '9Y3o3DOWT7eW_LTIg')
       .then((result) => {
@@ -13,17 +16,20 @@ export const ContactUs = () => {
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset()
+      
   };
 
+  const newLocal = 'submit ';
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="text" name="to_name" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+    <div className='contactme'>
+    <h2>Contact Me</h2>
+    <form ref={form} onSubmit={sendEmail} className='form-details'>
+      <input type="text" name="from_name" placeholder='Name'/>
+      <input type="text" name="to_name" placeholder='Email'/>
+      <textarea name="message" placeholder='Message' />
+      <input type="submit" value="Send" className={newLocal}/>
     </form>
+    </div>
   );
 };

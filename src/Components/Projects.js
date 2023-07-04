7fb8732a92f3ab88bd35ab84f1a0de281856a9d4
PrasadDa08 <prasaddatir05@gmail.com>
2023-07-04 06:00:44 +0530

    updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,57 +1,63 @@
 import React from 'react'
 import rps from "../images/Screenshot 2023-06-23 at 1.58.56 AM.png"
 import { Link } from 'react-router-dom'
+import{k} from '../icons/icons8-menu-50.png'
 function Projects() {
-  return (
-    <div className='projects'>
-      <h1>Projec<span>ts</span></h1>
-      <div className='project-content'>
+
+  const projects = [
+    {img:{rps}, title:'RPS',info:'A Simple game created by Using Python.'},
+    {img:{rps}, title:'RPS',info:'A Simple game created by Using Python.'}
+]
+  // return (
+    // <div className='projects'>
+    //   <h1>Projec<span>ts</span></h1>
+    //   <div className='project-content'>
       
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
 
-      <div className='project-card'>
-        <img src={rps}></img>
-        <h4>Rock, Paper, Sessiors Game</h4>
-        <p>A Simple game created by Using Python.</p>
-        <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
-      </div>
-    </div>
-    </div>
-  )
+    //   <div className='project-card'>
+    //     <img src={rps}></img>
+    //     <h4>Rock, Paper, Sessiors Game</h4>
+    //     <p>A Simple game created by Using Python.</p>
+    //     <a href="https://replit.com/@pRaSadd08/RockPapperScissors" target='_blank'>Replit Link</a>
+    //   </div>
+    // </div>
+    // </div>
+  // )
 }
 
 export default Projects

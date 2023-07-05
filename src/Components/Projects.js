fab5a57666a006dd695be804c311685273943ea5
PrasadDa08 <prasaddatir05@gmail.com>
2023-07-05 05:43:44 +0530

    Updated Projects Sestion

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import rps from "../images/Screenshot 2023-06-23 at 1.58.56 AM.png"
 import { Link } from 'react-router-dom'
-import{k} from '../icons/icons8-menu-50.png'
+import image1 from '../images/download.jpeg'
+import image2 from '../images/download (1).jpeg'
+import image3 from '../images/download (2).jpeg'
+import image4 from '../images/images.png'
+
+import ProjectItems from './ProjectItems'
 function Projects() {
 
   const projects = [
-    {img:{rps}, title:'RPS',info:'A Simple game created by Using Python.'},
-    {img:{rps}, title:'RPS',info:'A Simple game created by Using Python.'}
-]
-  // return (
+    {img:rps, title:'RPS',info:'A Simple game created by Using Python.'},
+    {img:image1, title:'RPS',info:'A Simple game created by Using Python.'},
+    {img:image2, title:'RPS',info:'A Simple game created by Using Python.'},
+    {img:image3, title:'RPS',info:'A Simple game created by Using Python.'},
+    {img:image4, title:'RPS',info:'A Simple game created by Using Python.'},
+  ]
+  return (
+    <ProjectItems projects = {projects}/>
     // <div className='projects'>
     //   <h1>Projec<span>ts</span></h1>
     //   <div className='project-content'>
@@ -57,7 +66,7 @@ function Projects() {
     //   </div>
     // </div>
     // </div>
-  // )
+  )
 }
 
 export default Projects

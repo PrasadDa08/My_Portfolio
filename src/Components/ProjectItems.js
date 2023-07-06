e3042160c0f2cf91399dc1fa89681da3cc4250bf
PrasadDa08 <prasaddatir05@gmail.com>
2023-07-06 05:50:55 +0530

    Updated Projects Sesction

--- src/Components/ProjectItems.js
+++ src/Components/ProjectItems.js
@@ -1,46 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function ProjectItems({projects}) {
+function ProjectItems({ projects }) {
+  const [currenIndex, setCurrentIndex] = useState(0);
 
-  const [currenIndex, setCurrentIndex] = useState(0)
+  const goToRight = () => {
+    const isLastProject = currenIndex === projects.length - 1;
+    const newIndex = isLastProject ? 0 : currenIndex + 1;
 
-  const goToRight = ()=>{
-    const isLastProject = currenIndex === projects.length - 1
-    const newIndex = isLastProject ? 0 : currenIndex + 1
-    
-    setCurrentIndex(newIndex)
-  } 
+    setCurrentIndex(newIndex);
+  };
 
-  const goToleft = ()=>{
-    const isFirstProject = currenIndex === 0
-    const newIndex = isFirstProject ? projects.length -1 : currenIndex - 1
-    
-    setCurrentIndex(newIndex)
-  } 
+  const goToleft = () => {
+    const isFirstProject = currenIndex === 0;
+    const newIndex = isFirstProject ? projects.length - 1 : currenIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
-  const goToProject = (projectIndex) =>{
-    setCurrentIndex(projectIndex)
-  }
-
+  const goToProject = (projectIndex) => {
+    setCurrentIndex(projectIndex);
+  };
 
   return (
-    <div  className='project-container'>
-      <div className='left-btn' onClick={goToleft}>〈 </div>
-      <div className='right-btn' onClick={goToRight}> 〉 </div>
-      <div className='image-container'>
-      <img src={projects[currenIndex===0 ?  projects.length -1 : currenIndex - 1].img } className='previous-img' />
-      <img src={projects[currenIndex].img} className='main-img'/>
-      <img src={projects[currenIndex === projects.length -1 ? 0 : currenIndex + 1].img} className='next-img'/>
-      </div>
-      <p>{projects[currenIndex].info}</p>
+    <>
+      <div className="project-container">
+        {/* Left-Right Buttons */}
+        <div className="left-btn" onClick={goToleft}>
+          ◀︎{" "}
+        </div>
+        <div className="right-btn" onClick={goToRight}>
+          {" "}
+          ►{" "}
+        </div>
 
-      <div className='project-dots'>
-        {projects.map((project, projectIndex) =>{
-          return <h1 onClick={()=> goToProject(projectIndex)}>{projectIndex === currenIndex?'●':'○'}</h1>
+        {/* Projects Images */}
+        <div className="image-container">
+          <img
+            src={
+              projects[
+                currenIndex === 0 ? projects.length - 1 : currenIndex - 1
+              ].img
+            }
+            className="previous-img"
+          />
+          <img src={projects[currenIndex].img} className="main-img" />
+          <img
+            src={
+              projects[
+                currenIndex === projects.length - 1 ? 0 : currenIndex + 1
+              ].img
+            }
+            className="next-img"
+          />
+        </div>
+
+        {/* Project Details */}
+        <div className="project-details">
+          <h3>{projects[currenIndex].title}</h3>
+          <p>{projects[currenIndex].info}</p>
+
+          {/* Project Links */}
+          <div className="project-links">
+            <p>GitHup</p>
+            <p>Replit</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="project-dots">
+        {projects.map((project, projectIndex) => {
+          return (
+            <h1 onClick={() => goToProject(projectIndex)}>
+              {projectIndex === currenIndex ? "●" : "○"}
+            </h1>
+          );
         })}
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default ProjectItems
+export default ProjectItems;
